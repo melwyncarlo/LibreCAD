11433a2fe8509c38aa86a746733e7d4a84b4bf38
Melwyn Francis Carlo <66683108+melwyncarlo@users.noreply.github.com>
2021-11-23 07:32:05 +0530

    Update qg_actionhandler.cpp

--- librecad/src/ui/qg_actionhandler.cpp
+++ librecad/src/ui/qg_actionhandler.cpp
@@ -788,14 +788,7 @@ RS_ActionInterface* QG_ActionHandler::setCurrentAction(RS2::ActionType id) {
 //        a = new RS_ActionSnapIntersectionManual(*document, *view);
 //        break;
     case RS2::ActionSnapPointLineIntersection:
-        {
-            const RS_Color redPenColor = RS_Color(255,0,0);
-            currentAppPen = new RS_Pen();
-           *currentAppPen = document->getActivePen();
-            const RS_Pen snapPointLineIntersection_pen = RS_Pen(redPenColor, RS2::Width01, RS2::DashDotLineTiny);
-            document->setActivePen(snapPointLineIntersection_pen);
-            a = new RS_ActionSnapPointLineIntersection(*document, *view);
-        }
+        a = new RS_ActionSnapPointLineIntersection(*document, *view);
         break;
 
         // Snap restriction actions:
